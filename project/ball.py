6cb3929b7166df8c8899ef6a97b692406e9a8ff5
guy18-meet <selaguy88@gmail.com>
2018-01-08 20:06:13 +0200

    project

---
+++ project/ball.py
@@ -0,0 +1,36 @@
+from turtle import Turtle
+class Ball:
+    def __init__(self,x,y,dx,dy,r,color):
+        Turtle.__init__(self)
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.r = r
+        self.color(color)
+        self.penup()#2
+        self.goto(x,y) #3
+        self.shape('circle')#5
+        self.shapesize(shape.r/10)#6
+        self.color(color)#7
+    def move(self,scree_width,screen_height):
+        current_x = self.xcor()#1
+        new_x = current_x + dx#2
+        current_y = self.ycor()
+        new_y = current_y + dy
+        
+        
+    
+        
+        
+        
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
