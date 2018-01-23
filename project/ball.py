a7062bdc0cab3ad30d4a11d906ef0ca4be892aab
guy18-meet <selaguy88@gmail.com>
2018-01-23 20:00:12 +0200

    hi

--- project/ball.py
+++ project/ball.py
@@ -15,15 +15,44 @@ class Ball:
         self.color(color)#7
     def move(self,screen_width,screen_height):
         current_x = self.xcor()#1
-        new_x = current_x + dx#2
         current_y = self.ycor()
-        new_y = current_y +dy#,3
-        
-        new_x = current_x + self.dx
-        new_y = current_y + self.dy#,3
-
                 
-    
+        new_x = current_x + self.dx#2,1 Get the x coordinates of the ball and save it inside a variable called current_x
+
+        new_y = current_y + self.dy#,3Get the y coordinates of the ball and save it inside a variable called current_y
+
+
+        right_side_ball = new_x + self.r#4 Get the right side of the ball (hint new x coordination + the radius) and save it inside a variable called right_side_ball
+
+        up_side_ball = new_y+ self.r#
+        left_side_ball = new_x - self.r#
+        down_side_ball = new_y-self.r#
+        turtle.goto(new_x,new_y)#Let the ball go to the new x and y coordinates
+        if new_x > screen_width/2:#  right side if new_x is bigger than the screen_width/2, than bounce it up
+            dx = -dx
+        if  new_x < (-1)*screen_width/2:# left side, need explenation!!!!!!!
+            
+            dx = -dx
+        if new_y > screen_height/2:#up
+            dy = -dy#### need explenation
+        if new_y < (-1)*screen_height/2:# down
+            dy = -dy
+
+        ##########agario.py##################
+import turtle
+import time
+import random
+from ball import Ball
+            
+            
+            
+        
+        
+            
+        
+            
+            
+            
         
         
         
